dbdd7116c86b0adb8787b4ce33f1ecf04f716c9f
Norawit Urailertprasert <wit543@gmail.com>
2020-02-28 12:48:28 +0700

    update theme path

--- install.sh
+++ install.sh
@@ -70,7 +70,7 @@ gitClone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/pl
 gitClone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 
 echo "Installing PowerLevel10K"
-gitClone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+gitClone https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/themes/powerlevel10k
 
 echo "Installing fzf"
 gitClone https://github.com/junegunn/fzf.git ~/.fzf
